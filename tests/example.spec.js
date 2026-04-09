896549ab640adc2c4cb8e40bcb1c333d5002376b
asavela-dlisani <asavela.dlisani@condorgreen.com>
2026-04-09 05:29:27 +0200

    test case id

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,7 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('TC-101 user can log in', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
