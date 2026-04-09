134cace4775728fa97a159ab46b7723808dc4deb
asavela-dlisani <asavela.dlisani@condorgreen.com>
2026-04-09 20:31:51 +0200

    Latest tests

--- tests/authentication.spec.js
+++ tests/authentication.spec.js
@@ -1,7 +1,7 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('Valid credentials', async ({ page }) => {
+test('Login with valid credentials', async ({ page }) => {
   await page.goto('http://localhost:8080/login');
   await expect(page.getByRole('heading', { name: 'Track, test, and deliver' })).toBeVisible();
 
@@ -11,10 +11,35 @@ test('Valid credentials', async ({ page }) => {
   await expect(page.getByRole('link', { name: 'Dashboard' })).toBeVisible();
 });
 
-test('Invalid credentials', async ({ page }) => {
+test.skip('Login with invalid credentials @pass', async ({ page }) => {
   await page.goto('http://localhost:8080/login');
   await page.getByRole('textbox', { name: 'Email' }).fill('wrong@gmail.com');
   await page.getByRole('textbox', { name: 'Password' }).fill('password');
   await page.getByRole('button', { name: 'Sign in' }).click();
   await expect(page.getByText('Invalid credentials')).toBeVisible();
+});
+
+test('Login with invalid credentials @fail', async ({ page }) => {
+  await page.goto('http://localhost:8080/login');
+  await page.getByRole('textbox', { name: 'Email' }).fill('invaliduser@gmail.com');
+  await page.getByRole('textbox', { name: 'Password' }).fill('password');
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await expect(page.getByText('Invalid credentials')).toBeVisible();
+});
+
+test('User can register', async ({ page }) => {
+  await page.goto('http://localhost:8080/login');
+  await expect(page.getByRole('heading', { name: 'Track, test, and deliver' })).toBeVisible();
+
+  await page.getByRole('link', { name: 'Sign up' }).click();
+  await expect(page.getByRole('heading', { name: 'Start managing quality today.' })).toBeVisible();
+
+  await page.getByRole('textbox', { name: 'Full Name' }).click();
+  await page.getByRole('textbox', { name: 'Full Name' }).fill('Invalid User');
+  await page.getByRole('textbox', { name: 'Email' }).fill('invaliduser@gmail.com');
+  await page.getByRole('textbox', { name: 'Password', exact: true }).fill('password');
+  await page.getByRole('textbox', { name: 'Confirm Password' }).click();
+  await page.getByRole('textbox', { name: 'Confirm Password' }).fill('password');
+  await page.getByRole('button', { name: 'Create account' }).click();
+  await expect(page.getByRole('link', { name: 'Dashboard' })).toBeVisible();
 });
